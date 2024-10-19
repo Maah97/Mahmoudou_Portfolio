11523b77826ee91cf1fad281381cd6e41155e076
Maah97 <mahmoudouaboul@gmail.com>
2024-10-19 18:21:13 +0100

    aae

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Projet from './pages/projet';
 import Error from './pages/error'
@@ -11,7 +11,7 @@ import './styles_SCSS/app.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router >
       <Header />
       <Routes >
         <Route path="/" element={<Home />} />
