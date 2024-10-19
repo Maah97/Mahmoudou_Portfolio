8a7016b2b428fd20c6b3d4c034d620b7db813254
Maah97 <mahmoudouaboul@gmail.com>
2024-10-19 16:26:22 +0100

    first commit

---
+++ src/components/cardProjet.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useMemo } from "react";
+import { Link } from 'react-router-dom';
+import MongoDB from '../assets/mongodb2.svg';
+import Lighthouse from '../assets/lighthouse2.svg'
+import Notion from '../assets/notion-logo.svg'
+
+function CardProjet(props) {
+    const containRef = useRef(null);
+    const ratio = 0.1
+    const options = useMemo(() => {
+        return {
+            root: null,
+            rootMargin: "0px",
+            threshold: ratio
+        };
+    }, [])
+     
+    const handleIntersect = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > ratio) {
+                entry.target.classList.add('visible-card-projet');
+                observer.unobserve(entry.target);
+            }
+        });
+    }
+    useEffect(() => {
+        const observer = new IntersectionObserver(handleIntersect, options);
+        let contain = containRef.current
+        if (containRef.current) {
+            observer.observe(contain);
+        }
+        return () => {
+            observer.unobserve(contain);
+        }
+    }, [containRef, options]);
+    return (
+        <Link ref={containRef} to={'/projet/'+ props.id} className="card-projet">
+            <div className='conteneur-card-projet'>
+                <img src={props.imgCover} alt="site booki" className='img-cover' />
+                <p>{props.titre}</p>
+                <div className="ligne"></div>
+                <p>{props.category}</p>
+                <div className='technos'>
+                    {
+                        props.technos.map((techno,i) => (
+                            <div key={i} className='techno'>
+                                {techno === "MongoDB" ? <img src={MongoDB} alt="logo MongoDB" /> : techno === "Lighthouse" ? <img src={Lighthouse} alt="logo MongoDB" /> : techno === "Notion" ? <img src={Notion} alt="logo MongoDB" />
+                                : <i className={`fa-brands fa-${techno}`}></i>}
+                                <p>{techno}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="txt-hover"><p>Voir le projet</p></div>
+        </Link>
+    )
+};
+
+export default CardProjet;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Projet from './pages/projet';
+import Error from './pages/error'
+import Header from "./components/header";
+import Footer from "./components/footer";
+import './styles_SCSS/app.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL} >
+      <Header />
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/projet/:id" element={<Projet />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
