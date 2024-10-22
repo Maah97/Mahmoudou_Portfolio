2af595d4017f6c0ac8c3f710702eb18dd6dee09c
Maah97 <mahmoudouaboul@gmail.com>
2024-10-22 18:26:09 +0100

    zdzdz

--- src/components/cardProjet.jsx
+++ src/components/cardProjet.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
+import { NavHashLink } from "react-router-hash-link";
 import { useEffect, useRef, useMemo } from "react";
 import MongoDB from '../assets/mongodb2.svg';
 import Lighthouse from '../assets/lighthouse2.svg'
@@ -35,7 +36,7 @@ function CardProjet(props) {
         }
     }, [containRef, options]);
     return (
-        <HashLink ref={containRef} to={'/projects/' + props.id + '#project'} className="card-projet">
+        <NavHashLink ref={containRef} to={'/projects/' + props.id + '#project'} className="card-projet">
             <div className='conteneur-card-projet'>
                 <img src={props.imgCover} alt="site booki" className='img-cover' />
                 <p>{props.titre}</p>
@@ -54,7 +55,7 @@ function CardProjet(props) {
                 </div>
             </div>
             <div className="txt-hover"><p>Voir le projet</p></div>
-        </HashLink>
+        </NavHashLink>
     )
 };
 
